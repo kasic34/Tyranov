0786df24fe676c44b955593d14339315702918a3
kasic34 <162434470+kasic34@users.noreply.github.com>
2025-06-11 08:23:15 +0500

    Add files via upload

---
+++ Каминский/Каминский/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace Каминский 
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.PasswordAuth = new System.Windows.Forms.TextBox();
+            this.LoginAuth = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.PasswordReg2 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.PasswordReg = new System.Windows.Forms.TextBox();
+            this.FIO = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label12 = new System.Windows.Forms.Label();
+            this.LoginReg = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(10, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 25);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Логин";
+            // 
+            // PasswordAuth
+            // 
+            this.PasswordAuth.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PasswordAuth.Location = new System.Drawing.Point(52, 241);
+            this.PasswordAuth.Name = "PasswordAuth";
+            this.PasswordAuth.Size = new System.Drawing.Size(226, 31);
+            this.PasswordAuth.TabIndex = 11;
+            // 
+            // LoginAuth
+            // 
+            this.LoginAuth.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LoginAuth.Location = new System.Drawing.Point(52, 140);
+            this.LoginAuth.Name = "LoginAuth";
+            this.LoginAuth.Size = new System.Drawing.Size(226, 31);
+            this.LoginAuth.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(8, 358);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(310, 25);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Повторите введенный пароль";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(69, 213);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(86, 25);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Пароль";
+            // 
+            // PasswordReg2
+            // 
+            this.PasswordReg2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PasswordReg2.Location = new System.Drawing.Point(16, 401);
+            this.PasswordReg2.Name = "PasswordReg2";
+            this.PasswordReg2.Size = new System.Drawing.Size(226, 31);
+            this.PasswordReg2.TabIndex = 19;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(69, 106);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(71, 25);
+            this.label10.TabIndex = 30;
+            this.label10.Text = "Логин";
+            // 
+            // PasswordReg
+            // 
+            this.PasswordReg.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PasswordReg.Location = new System.Drawing.Point(14, 261);
+            this.PasswordReg.Name = "PasswordReg";
+            this.PasswordReg.Size = new System.Drawing.Size(226, 31);
+            this.PasswordReg.TabIndex = 31;
+            // 
+            // FIO
+            // 
+            this.FIO.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FIO.Location = new System.Drawing.Point(16, 70);
+            this.FIO.Name = "FIO";
+            this.FIO.Size = new System.Drawing.Size(222, 31);
+            this.FIO.TabIndex = 32;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(16, 467);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(285, 65);
+            this.button3.TabIndex = 34;
+            this.button3.Text = "Регистрация";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(52, 323);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(262, 109);
+            this.button4.TabIndex = 35;
+            this.button4.Text = "Войти";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label12.Location = new System.Drawing.Point(10, 227);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(86, 25);
+            this.label12.TabIndex = 36;
+            this.label12.Text = "Пароль";
+            // 
+            // LoginReg
+            // 
+            this.LoginReg.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LoginReg.Location = new System.Drawing.Point(14, 165);
+            this.LoginReg.Name = "LoginReg";
+            this.LoginReg.Size = new System.Drawing.Size(224, 31);
+            this.LoginReg.TabIndex = 38;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.Location = new System.Drawing.Point(20, 36);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(61, 25);
+            this.label11.TabIndex = 39;
+            this.label11.Text = "ФИО";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.LoginReg);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label12);
+            this.groupBox1.Controls.Add(this.PasswordReg2);
+            this.groupBox1.Controls.Add(this.PasswordReg);
+            this.groupBox1.Controls.Add(this.FIO);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(380, 548);
+            this.groupBox1.TabIndex = 40;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Регистрация";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.PasswordAuth);
+            this.groupBox2.Controls.Add(this.LoginAuth);
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox2.Location = new System.Drawing.Point(474, 26);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(350, 548);
+            this.groupBox2.TabIndex = 41;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Авторизация";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(836, 609);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox PasswordAuth;
+        private System.Windows.Forms.TextBox LoginAuth;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox PasswordReg2;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox PasswordReg;
+        private System.Windows.Forms.TextBox FIO;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox LoginReg;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ Каминский/Каминский/Form1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Каминский
+{
+    public partial class Form1 : Form
+    {
+        public static string ConnectString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Database1.mdf;Integrated Security=True";
+
+        // Цветовая палитра
+        private readonly Color primaryColor = Color.FromArgb(51, 51, 76);
+        private readonly Color secondaryColor = Color.FromArgb(231, 76, 60);
+        private readonly Color lightBackground = Color.FromArgb(240, 240, 240);
+        private readonly Color successColor = Color.FromArgb(46, 204, 113);
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+            ApplyDesign();
+
+            CenterControlsInGroupBox(groupBox1);
+            CenterControlsInGroupBox(groupBox2);
+        }
+
+        /// <summary>
+        /// Применяет дизайн ко всем элементам управления
+        /// </summary>
+        private void ApplyDesign()
+        {
+            // Настройка основной формы
+            this.BackColor = lightBackground;
+            this.Padding = new Padding(10);
+            this.Text = "Система авторизации";
+            this.Font = new Font("Times New Roman", 14, FontStyle.Regular); 
+
+            // GroupBox
+            GroupBox[] groups = { groupBox1, groupBox2 };
+            foreach (var group in groups)
+            {
+                group.BackColor = Color.White;
+                group.ForeColor = primaryColor;
+                group.Font = new Font("Times New Roman", 14, FontStyle.Bold); 
+                group.Padding = new Padding(10);
+                group.FlatStyle = FlatStyle.Flat;
+            }
+
+            // Стили Label
+            foreach (Control control in this.Controls)
+            {
+                if (control is Label lbl)
+                {
+                    lbl.ForeColor = primaryColor;
+                    lbl.Font = new Font("Times New Roman", 14, FontStyle.Regular); 
+                }
+            }
+
+            // Стили TextBox
+            foreach (Control control in this.Controls)
+            {
+                if (control is TextBox textBox)
+                {
+                    textBox.BorderStyle = BorderStyle.FixedSingle;
+                    textBox.BackColor = Color.White;
+                    textBox.Font = new Font("Times New Roman", 14); 
+                    textBox.ForeColor = Color.Black;
+                    textBox.Margin = new Padding(5);
+                }
+            }
+
+            // Кнопка регистрации
+            StyleButton(button3, primaryColor, Color.FromArgb(123, 123, 123));
+
+            // Кнопка входа
+            StyleButton(button4, secondaryColor, Color.FromArgb(251, 96, 80));
+        }
+
+
+        private void StyleButton(Button btn, Color baseColor, Color hoverColor)
+        {
+            btn.FlatStyle = FlatStyle.Flat;
+            btn.FlatAppearance.BorderSize = 0;
+            btn.BackColor = baseColor;
+            btn.ForeColor = Color.White;
+            btn.Font = new Font("Times New Roman", 14, FontStyle.Bold); 
+            btn.Cursor = Cursors.Hand;
+            btn.Height = 40;
+            btn.Width = 140;
+            btn.Margin = new Padding(5);
+            btn.MouseEnter += (s, e) => btn.BackColor = hoverColor;
+            btn.MouseLeave += (s, e) => btn.BackColor = baseColor;
+        }
+
+
+        /// <summary>
+        /// Хэширование строки с использованием MD5
+        /// </summary>
+        public string md5(string input)
+        {
+            using (MD5 md5Hash = MD5.Create())
+            {
+                byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+                StringBuilder builder = new StringBuilder();
+                foreach (byte b in data)
+                {
+                    builder.Append(b.ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        // ===== РЕГИСТРАЦИЯ =====
+       
+
+        // ===== АВТОРИЗАЦИЯ =====
+       
+
+        /// <summary>
+        /// Показывает сообщение с заданными параметрами
+        /// </summary>
+        private void ShowMessage(string text, string caption, MessageBoxIcon icon)
+        {
+            MessageBox.Show(text, caption, MessageBoxButtons.OK, icon);
+        }
+
+        /// <summary>
+        /// Очищает поля регистрации
+        /// </summary>
+        private void ClearRegistrationFields()
+        {
+            FIO.Clear();
+            LoginReg.Clear();
+            PasswordReg.Clear();
+            PasswordReg2.Clear();
+        }
+
+        /// <summary>
+        /// Очищает поля авторизации
+        /// </summary>
+        private void ClearAuthFields()
+        {
+            LoginAuth.Clear();
+            PasswordAuth.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Валидация полей
+            if (string.IsNullOrWhiteSpace(FIO.Text) ||
+                string.IsNullOrWhiteSpace(LoginReg.Text) ||
+                string.IsNullOrWhiteSpace(PasswordReg.Text))
+            {
+                ShowMessage("Все поля обязательны для заполнения", "Ошибка ввода", MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (PasswordReg.Text != PasswordReg2.Text)
+            {
+                ShowMessage("Пароли не совпадают", "Ошибка проверки", MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(ConnectString))
+                {
+                    connection.Open();
+
+                    string query = "INSERT INTO [Users] ([FIO], [Login], [Password]) VALUES (@fio, @login, @password)";
+                    SqlCommand command = new SqlCommand(query, connection);
+
+                    command.Parameters.AddWithValue("@fio", FIO.Text.Trim());
+                    command.Parameters.AddWithValue("@login", LoginReg.Text.Trim());
+                    command.Parameters.AddWithValue("@password", md5(PasswordReg.Text));
+
+                    int rowsAffected = command.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        ShowMessage("Регистрация прошла успешно!", "Успех", MessageBoxIcon.Information);
+                        ClearRegistrationFields();
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                ShowMessage($"Ошибка базы данных:\n{ex.Message}", "Ошибка регистрации", MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                ShowMessage($"Непредвиденная ошибка:\n{ex.Message}", "Ошибка", MessageBoxIcon.Error);
+            }
+        }
+
+        private void CenterControlsInGroupBox(GroupBox groupBox)
+        {
+            foreach (Control control in groupBox.Controls)
+            {
+                // Центрирование по горизонтали относительно GroupBox
+                control.Left = (groupBox.ClientSize.Width - control.Width) / 2;
+            }
+        }
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(LoginAuth.Text) ||
+               string.IsNullOrWhiteSpace(PasswordAuth.Text))
+            {
+                ShowMessage("Введите логин и пароль", "Ошибка ввода", MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(ConnectString))
+                {
+                    connection.Open();
+
+                    string query = "SELECT [FIO], [Login], [Password], [role] FROM [Users] WHERE [Login] = @login";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@login", LoginAuth.Text.Trim());
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            string storedHash = reader["Password"].ToString();
+                            if (storedHash == md5(PasswordAuth.Text))
+                            {
+                                string fio = reader["FIO"].ToString();
+                                string role = reader["role"].ToString();
+
+                                ShowMessage($"Добро пожаловать, {fio}!\nВаша роль: {role}",
+                                           "Авторизация успешна",
+                                           MessageBoxIcon.Information);
+                                ClearAuthFields();
+                            }
+                            else
+                            {
+                                ShowMessage("Неверный пароль", "Ошибка авторизации", MessageBoxIcon.Warning);
+                            }
+                        }
+                        else
+                        {
+                            ShowMessage("Пользователь не найден", "Ошибка авторизации", MessageBoxIcon.Warning);
+                        }
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                ShowMessage($"Ошибка базы данных:\n{ex.Message}", "Ошибка авторизации", MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                ShowMessage($"Непредвиденная ошибка:\n{ex.Message}", "Ошибка", MessageBoxIcon.Error);
+            }
+        }
+    }
+}
